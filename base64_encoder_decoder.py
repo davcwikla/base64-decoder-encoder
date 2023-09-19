2b0ba5cced772329d69f11e7ccfbf5fe9d7d4630
David <94928782+davcwikla@users.noreply.github.com>
2023-09-19 11:43:35 +0200

    Add files via upload

--- base64_encoder_decoder.py
+++ base64_encoder_decoder.py
@@ -18,4 +18,4 @@ decoded_cookie_bytes = decoded_cookie_string.encode('ascii')
 encoded_cookie = base64.b64encode(decoded_cookie_bytes)
 encoded_cookie_string = encoded_cookie.decode('ascii')
 
-print('Decoded value is = ', encoded_cookie_string)
+print('Encoded value in base64 is: ', encoded_cookie_string)
